ef79714ce6dc913614a7afe0c885f01c15d4e424
asteph <aliciap.stephens@gmail.com>
2020-12-22 15:03:11 +0000

    move

--- Power.java
+++ Power.java
@@ -1,1 +1,19 @@
-String[] stringArray2 = new String[4]
+import java.lang.Math; 
+  
+public class Power { 
+  
+    // Function to calculate N raised to the power P 
+    static double power(int N, int P) 
+    { 
+        return Math.pow(N, P); 
+    } 
+  
+    // Driver code 
+    public static void main(String[] args) 
+    { 
+        int N = 4; 
+        int P = 2; 
+  
+        System.out.println(power(N, P)); 
+    } 
+} 
